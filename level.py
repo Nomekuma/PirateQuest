534fbe835c70d2a9cd654e1b7a9bd220d4353560
github username <t.gunness35@gmail.com>
2023-03-29 23:09:38 +0400

    Feat:collision

--- level.py
+++ level.py
@@ -1,10 +1,10 @@
 import pygame
 from world_tile import tile
-from settings import tile_size
+from settings import tile_size,width
 from player import Player
 
 class level(): # No need to inherit from pygame.sprite.Sprite/But we need __init__ method
-    def __init__(self,level_data,surface):
+    def __init__(self,level_data,surface):# level_data is the level map
         # Level setup
         self.display_surface=surface
         #self.level_data=level_data # No need set in seperate attribute
@@ -12,7 +12,7 @@ class level(): # No need to inherit from pygame.sprite.Sprite/But we need __init
         self.whorld_shift=0 # The amount of pixels the player moves
 
     
-    def setup_level(self,layout):
+    def setup_level(self,layout):# layout is the level map
         self.tiles=pygame.sprite.Group() # Create a group of tiles
         self.player=pygame.sprite.GroupSingle()# Create a group of player
 
@@ -34,7 +34,48 @@ class level(): # No need to inherit from pygame.sprite.Sprite/But we need __init
                     player_sprite=Player((x,y))
                     self.player.add(player_sprite)
 
-                
+    def horizontal_movement_collision(self):
+        player=self.player.sprite
+        player.rect.x += player.direction.x * player.speed # Move the player 5 pixels to the left/right
+        # Check for collision with the level tiles
+        for sprite in self.tiles.sprites():# Loop through all the tiles
+            if sprite.rect.colliderect(player.rect):# If the player collides with a tile
+                if player.direction.x > 0:# If the player is moving right
+                    player.rect.right = sprite.rect.left# Move the player to the left of the tile
+                elif player.direction.x < 0: # If the player is moving left
+                    player.rect.left = sprite.rect.right # Move the player to the right of the tile
+
+    def vertical_movement_collision(self):
+        player=self.player.sprite
+        player.apply_gravity() # Apply gravity method # Move the player 5 pixels up/down
+        # Check for collision with the level tiles
+        for sprite in self.tiles.sprites():# Loop through all the tiles
+            if player.rect.colliderect(sprite.rect):# If the player collides with a tile
+                if player.direction.y > 0:# If the player is moving down
+                    player.rect.bottom = sprite.rect.top# Move the player to the top of the tile
+                    player.direction.y = 0 # Stop the player from moving down
+                elif player.direction.y < 0:# If the player is moving up
+                    player.rect.top = sprite.rect.bottom # Move the player to the bottom of the tile
+                    player.direction.y = 0 # Stop the player from moving up
+
+
+    def scroll_x(self):# Scroll the level in the x direction
+        player=self.player.sprite
+        player_x=player.rect.centerx
+        direction_x=player.direction.x    
+         
+        # Scroll the level in the x direction
+        # if player_x <200 and direction_x<0:# If the player is less than 200 pixels from the left side of the screen
+        if player_x <width*0.75 and direction_x<0:
+            self.whorld_shift=5
+            player.speed=0    
+        # elif player_x >400 and direction_x>0:# If the player is more than 400 pixels from the left side of the screen
+        elif player_x >width - (width*0.75) and direction_x>0:   
+            self.whorld_shift=-5
+            player.speed=0
+        else:
+            self.whorld_shift=0
+            player.speed=5
             
      
     #  Run method
@@ -42,10 +83,13 @@ class level(): # No need to inherit from pygame.sprite.Sprite/But we need __init
         # level tiles
         self.tiles.update(self.whorld_shift)# x_shift is the amount of pixels the player moves
         self.tiles.draw(self.display_surface)# Draw the tiles
+        self.scroll_x()# Scroll the level in the x direction
 
 
         # player
         self.player.update()# Update the player
+        self.horizontal_movement_collision()# Check for collision with the level tiles
+        self.vertical_movement_collision()# Check for collision with the level tiles
         self.player.draw(self.display_surface) # Draw the player
 
         
--- player.py
+++ player.py
@@ -8,20 +8,37 @@ class Player(pygame.sprite.Sprite):
         self.image=pygame.Surface((20,50)) #20 pixels wide, 50 pixels tall
         self.image.fill(('red'))# Player-Example-red
         self.rect=self.image.get_rect(topleft=pos)#Top left = were player spawns
+        
+        # Player attributes/movement
         self.direction= pygame.math.Vector2(0,0)# Direction the player is moving(vector)
+        self.speed=5 # Speed of the player
+        self.gravity=0.5 # Gravity of the player
+        self.jump_speed=-5 # Is the player jumping
 
 
     # Get input method
     def get_input(self):
         keys=pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT]:# If the left key is pressed
           self.direction.x=-1
-        elif keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_RIGHT]:# If the right key is pressed
             self.direction.x=1
         else:
             self.direction.x=0
+        if keys[pygame.K_SPACE]:# If the space key is pressed
+            self.jump()
+    # Apply gravity method
+    def apply_gravity(self):
+        self.direction.y+=self.gravity# Add gravity to the player
+        self.rect.y+=self.direction.y# Move the player down by the gravity amount
+    
+    # Jump method
+    def jump(self):
+        self.direction.y=self.jump_speed# Set the direction.y to the jump speed
+        
 
+    
     # Update method
     def update(self):
         self.get_input() # Get input method
-        self.rect.x += self.direction.x * 5 # Move the player 5 pixels to the left/right
+        
